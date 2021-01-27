651895d1e0c85739196ac74a63733de266107c6e
kanatamon <ponde.moonlord@gmail.com>
2021-01-27 22:56:58 +0700

    reimplement with solution 2

--- lib/02_word_count.exs
+++ lib/02_word_count.exs
@@ -1,45 +1,53 @@
-# TODO: Finish the challenge 2 on https://alchemist.camp/episodes/word-count
-
 # `a \> b` this syntax is called "Pipeline"
 # it make value of `a` as the first argument of `b` function
 filename =
-  IO.gets("File to operate: ")
+  IO.gets("File to count the words from (h for help):\n ")
   |> String.trim()
 
-operation =
-  IO.gets("Operations
-  [1] - Count words
-  [2] - Count lines
-  [3] - Count characters including numberic
-Type operation: ")
-  |> String.trim()
+if filename == "h" do
+  IO.puts("""
+  Usage: [filename] [-flags]
+  Flags:
+  -l    displays line count
+  -c    displays character count
+  -w    displays word count (default)
+  Multiply flags may be used. Exampler usage to display line and character count:
 
-# `!` in below is a `bang`
-# normally calling `File.read()` will return either ok or error
-# but with `!`(bang) this will force function to throw error
-body = File.read!(filename)
+  somefile.txt -lc
 
-case operation do
-  "1" ->
-    body
-    # Note: the `~r` is regex
-    #  , split by none-word and apostrophe `[^\w']` or new line `\\n`
-    |> String.split(~r{(\\n|[^\w'])+})
-    # Note: `fn x -> x != "" end` this is a anonymous function
+  """)
+else
+  parts = String.split(filename, "-")
+  filename = List.first(parts) |> String.trim()
+
+  flags =
+    case Enum.at(parts, 1) do
+      nil -> ["w"]
+      chars -> String.split(chars, "") |> Enum.filter(fn x -> x != "" end)
+    end
+
+  # `!` in below is a `bang`
+  # normally calling `File.read()` will return either ok or error
+  # but with `!`(bang) this will force function to throw error
+  body = File.read!(filename)
+
+  # WHAT?: \r\n | \r
+  lines = String.split(body, ~r{(\r\n|\n|\r)})
+
+  # Note: the `~r` is regex
+  #  , split by none-word and apostrophe `[^\w']` or new line `\\n`
+  words =
+    String.split(body, ~r{(\\n|[^\w'])+})
     |> Enum.filter(fn x -> x != "" end)
-    |> Enum.count()
-    |> IO.puts()
 
-  "2" ->
-    body
-    |> String.split(~r{\n})
-    |> Enum.count()
-    |> IO.puts()
+  chars = String.split(body, "") |> Enum.filter(fn x -> x != "" end)
 
-  "3" ->
-    body
-    |> String.graphemes()
-    |> Enum.filter(fn x -> String.match?(x, ~r{(\w|\d)}) end)
-    |> Enum.count()
-    |> IO.puts()
+  Enum.each(flags, fn flag ->
+    case flag do
+      "l" -> IO.puts("Lines: #{Enum.count(lines)}")
+      "w" -> IO.puts("Words: #{Enum.count(words)}")
+      "c" -> IO.puts("Chars: #{Enum.count(chars)}")
+      _ -> nil
+    end
+  end)
 end
